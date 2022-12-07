24b142796236a6f3556c93fe4f4e3e557006c7f0
Cameron505 <47005239+Cameron505@users.noreply.github.com>
2022-12-07 14:49:26 -0500

    respiration update

--- Code/Respiration.R
+++ Code/Respiration.R
@@ -11,14 +11,29 @@ A$Inc.temp<- as.factor(A$Inc.temp)
 PRes<-ggplot(A, aes(x=Inc.temp, y=Res, color=pre.inc))+
   stat_summary(fun = mean,geom = "point",lwd=4) +
   stat_summary(fun.data = mean_se, geom = "errorbar", lwd=1, width = 0.5)+
-  facet_wrap(~Date)+
+  facet_wrap(~Day)+
   theme_light()+
   scale_colour_manual(values=cbPalette)+
   scale_fill_manual(values=cbPalette)+
   ylab(expression(paste( "Respiration (",mu,"g-C",Hr^-1, ")")))+
   xlab("incubation temperature")+
   labs(color='pre.inc temp') +
-  theme_CKM()+
+  theme(legend.direction = 'vertical', 
+        legend.key = element_rect(size=2),
+        legend.key.size = unit(3, 'lines'),
+        panel.grid.major = element_blank(), 
+        legend.title=element_text(size=30,colour='black'),
+        panel.grid.minor = element_blank(),
+        legend.text=element_text(size=25),
+        axis.title.y= element_text(size=30, colour='black'),
+        axis.text.y = element_text(size=20, colour='black'),
+        axis.ticks = element_line(colour = "black", size=2),
+        axis.ticks.length = unit(0.3, "cm"),
+        axis.title.x=element_text(size=30, colour='black'),
+        axis.text.x = element_text(size=20, colour='black'),
+        panel.border = element_rect(colour = "black", fill=NA, size=2),
+        plot.title = element_text( size=30, face="bold.italic"),
+        plot.margin=unit(c(1,1,1,1), "cm"))+
   ggtitle("Soil Respiration")
 
 ggsave('Graphs/Respiration each day_Antecedent Temp.png', plot=PRes, width= 20, height= 12)
@@ -38,3 +53,22 @@ PRes2<-ggplot(A, aes(x=Date, y=Res, color=pre.inc))+
   ggtitle("Soil Respiration")
 
 ggsave('Graphs/Respiration each temperature Temp.png', plot=PRes2, width= 20, height= 12)
+
+
+
+theme(legend.direction = 'vertical', 
+      legend.key = element_rect(size=2),
+      legend.key.size = unit(3, 'lines'),
+      panel.grid.major = element_blank(), 
+      legend.title=element_text(size=30,colour='black'),
+      panel.grid.minor = element_blank(),
+      legend.text=element_text(size=25),
+      axis.title.y= element_text(size=30, colour='black'),
+      axis.text.y = element_text(size=20, colour='black'),
+      axis.ticks = element_line(colour = "black", size=2),
+      axis.ticks.length = unit(0.3, "cm"),
+      axis.title.x=element_text(size=30, colour='black'),
+      axis.text.x = element_text(size=20, colour='black'),
+      panel.border = element_rect(colour = "black", fill=NA, size=2),
+      plot.title = element_text( size=30, face="bold.italic"),
+      plot.margin=unit(c(1,1,1,1), "cm"))
