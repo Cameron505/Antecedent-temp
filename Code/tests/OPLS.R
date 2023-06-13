ce721581e7129161e7b3ad8b81896c4095f8d19e
Cameron505 <47005239+Cameron505@users.noreply.github.com>
2023-06-13 12:30:13 -0700

    PLS and OPLS
    PLS and OPLS seem to be working, though I am still not sure how to get the data out of these objects to put into a pretty plot. Also OPLS can only be applied to two factors, so PLS must be used for incubation temp sep.

--- Code/tests/OPLS.R
+++ Code/tests/OPLS.R
@@ -1,3 +1,6 @@
+library(ropls)
+
+#practice data provided
 data(foods) ## see Eriksson et al. (2001); presence of 3 missing values (NA)
 head(foods)
 foodMN <- as.matrix(foods[, colnames(foods) != "Country"])
@@ -43,26 +46,56 @@ sacSet <- getEset(sacPlsda)
 head(Biobase::pData(sacSet))
 head(Biobase::fData(sacSet))
 
+####MY DATA (Need to tarload)
+fticr_data_longform = FTICR_processed$fticr_data_longform_combined
+fticr_meta  = FTICR_processed$fticr_meta_combined
+TREATMENTS = dplyr::quos(pre,inc,Polar)
+TREATMENTS2 = dplyr::quos(pre,inc)
+relabund_cores = 
+  fticr_data_longform %>%
+  compute_relabund_cores(fticr_meta, TREATMENTS)%>%
+  filter(inc!='NA')
+relabund_wide = 
+  relabund_cores %>% 
+  ungroup() %>% 
+  mutate(Class = factor(Class, 
+                        levels = c("aliphatic", "unsaturated/lignin", 
+                                   "aromatic", "condensed aromatic"))) %>% 
+  dplyr::select(-c(abund, total)) %>% 
+  spread(Class, relabund) %>% 
+  replace(is.na(.), 0)
+
+
+
+##Data reorganization
+nameH<-c(1:215)
+
 Data_OPLS<- relabund_wide %>%
   select(CoreID,aliphatic,`unsaturated/lignin`,aromatic,`condensed aromatic`)
 Data_OPLS<-as.data.frame(Data_OPLS)
 Data_OPLS_1 <- as.matrix(Data_OPLS[, colnames(Data_OPLS) != "CoreID"])
-rownames(Data_OPLS_1) <- Data_OPLS[ ,"CoreID"]
+rownames(Data_OPLS_1) <- nameH
 
 Meta_OPLS<-relabund_wide %>%
   select(-c(aliphatic,`unsaturated/lignin`,aromatic,`condensed aromatic`))
 Meta_OPLS<-as.data.frame(Meta_OPLS)
 Meta_OPLS_1 <- as.matrix(Meta_OPLS[, colnames(Meta_OPLS) != "CoreID"])
-rownames(Meta_OPLS_1) <- Meta_OPLS[ ,"CoreID"]
+rownames(Meta_OPLS_1) <- nameH
 
-OPLS_1<-opls(Data_OPLS_1, Meta_OPLS[, "pre"])
+##PLS-DA
+PLS_DA_all = opls(Data_OPLS_1,Meta_OPLS[, "pre"])
+
+plot(PLS_DA_all, typeVc = "x-score")
+
+PLS_DA_all2 = opls(Data_OPLS_1,Meta_OPLS[, "inc"]) #Does not work
+
+plot(PLS_DA_all2, typeVc = "x-score")
+
+PLS_DA_all@scoreMN #I think these are the coords for the PLS-DA plot? where is class data stored...Also I cant seem to call an individual vector from this object...  
 
 
-VIP<-getVipVn(OPLS_1)
+##OPLS-DA "OPLS-DA only available for binary classification (use PLS-DA for multiple classes)"
+OPLS_DA_all = opls(Data_OPLS_1,Meta_OPLS[, "pre"],predI = 1, orthoI = NA)
 
-is.numeric(Data_OPLS)
-class(Data_OPLS)
-
-pca_all = opls(Data_OPLS_1,Meta_OPLS[, "pre"])
-
-plot(OPLS_1, typeVc = "x-score")
+plot(OPLS_DA_all, typeVc = "x-score")
+#Not sure where the coordinates are for this OPLS-DA within the object
